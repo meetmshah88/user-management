34ee29ae9bb6331b854dc57291ba55ee8cd21d07
meetmshah88 <meetmshah88@gmail.com>
2024-11-13 17:32:46 +0530

    update in header

--- frontend/src/constant/data.ts
+++ frontend/src/constant/data.ts
@@ -4,3 +4,5 @@ export const API_BASE = 'http://localhost:5000';
 // Regex patterns
 export const namePattern = /^[A-Za-z]+$/;
 export const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const SAPIENS_LOGO = "https://media.licdn.com/dms/image/v2/C4D0BAQH0HFifNAdaeg/company-logo_200_200/company-logo_200_200/0/1648377868644/sapiens_logo?e=2147483647&v=beta&t=fUFin0QP0yr6-eyE6CMAg0VZRaQeXr830ZoO5_zQEEY"
